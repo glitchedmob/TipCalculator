1156e2eb6d2e64749d96e988f9766e5165397ded
Levi Zitting <me@levizitting.com>
2019-09-30 14:01:29 -0500

    Add CalculatorPage

--- TipCalculator/App.xaml.cs
+++ TipCalculator/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TipCalculator.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace TipCalculator
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new CalculatorPage();
         }
 
         protected override void OnStart()
